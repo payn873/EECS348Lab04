714c16397057eacb1757a0b6c1e730a280db1f21
payn873 <142923202+payn873@users.noreply.github.com>
2024-10-04 15:57:55 -0500

    Update code2.c
    Added address operators to the variables being inputted to in scanf

--- code2.c
+++ code2.c
@@ -5,17 +5,17 @@ int main(){
 	float temp;
 	char scale;
 	printf("Enter what temperature type you are inputing (e.x. f, c, k): ");
-	scanf(" %c", type);
+	scanf(" %c", &type);
 	while(type != 'f' && type != 'c' && type != 'k'){
 		printf("You entered an invalid temperature type. Please enter f for farenheit, c for celsius, or k for kelvin :) : ");
-		scanf(" %c", type);
+		scanf(" %c", &type);
 	}
 	printf("Enter the temperature value (float): ");
-	scanf("%f", temp);
+	scanf("%f", &temp);
 	printf("Enter what temperature type you wish to conver to (e.x. f, c, k): ");
-	scanf(" %c", scale);
+	scanf(" %c", &scale);
 	while((scale != 'f' && scale != 'c' && scale != 'k') || scale == type){
 		printf("You entered an invalid temperature type. Pleae enter f for farenheit, c for celsius, or k for kelvin, and remember to make sure this type is different from the previously entered type: ");
-		scanf(" %c", scale);
+		scanf(" %c", &scale);
 	}
 }
