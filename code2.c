6c39aee78ff5c69754b5304d06466dead4061535
payn873 <142923202+payn873@users.noreply.github.com>
2024-10-04 20:42:47 -0500

    Update code2.c
    Added a categorize temperature function to comment on the weather.

--- code2.c
+++ code2.c
@@ -10,6 +10,7 @@ int main(){
 	float celsius_to_kelvin(float);
 	float farenheit_to_kelvin(float);
 	float kelvin_to_farenheit(float);
+	void categorize_temperature(float);
 	printf("Enter what temperature type you are inputing (e.x. f, c, k): ");
 	scanf(" %c", &type);
 	while(type != 'f' && type != 'c' && type != 'k'){
@@ -26,26 +27,32 @@ int main(){
 	}
 	if(type == 'f'){
 		if(scale == 'c'){
-			printf("%f\n", farenheit_to_celsius(temp));
+			printf("Converted Temperature: %f Degrees Celsius\n", farenheit_to_celsius(temp));
+			categorize_temperature(farenheit_to_celsius(temp));
 		}
 		else{
-			printf("%f\n", farenheit_to_kelvin(temp));
+			printf("Converted Temperature: %f Kelvin\n", farenheit_to_kelvin(temp));
+			categorize_temperature(farenheit_to_celsius(temp));
 		}
 	}
 	else if(type == 'c'){
 		if(scale == 'k'){
-			printf("%f\n", celsius_to_kelvin(temp));
+			printf("Converted Temperature: %f Kelvin\n", celsius_to_kelvin(temp));
+			categorize_temperature(temp);
 		}
 		else{
-			printf("%f\n", celsius_to_farenheit(temp));
+			printf("Converted Temperature: %f Degrees Farenheit\n", celsius_to_farenheit(temp));
+			categorize_temperature(temp);
 		}
 	}
 	else{
 		if(scale == 'f'){
-			printf("%f\n", kelvin_to_farenheit(temp));
+			printf("Converted Temperature: %f Degrees Farenheit\n", kelvin_to_farenheit(temp));
+			categorize_temperature(kelvin_to_celsius(temp));
 		}
 		else{
-			printf("%f\n", kelvin_to_celsius(temp));
+			printf("Converted Temperature: %f Degrees Celsius\n", kelvin_to_celsius(temp));
+			categorize_temperature(kelvin_to_celsius(temp));
 		}
 	}
 }
@@ -80,4 +87,22 @@ float farenheit_to_kelvin(float farenheit){
 	float celsius = farenheit_to_celsius(farenheit);
 	float kelvin = celsius_to_kelvin(celsius);
 	return kelvin;
+}
+
+void categorize_temperature(float celsius){
+	if(celsius > 35.0){
+		printf("Temperature Category: Extreme Heat\nWeather Advisory: Stay inside or go out with the polar bears.\n");
+	}
+	else if(celsius <= 35.0 && celsius > 25.0){
+		printf("Temperature Category: Hot\nWeather Advisory: Wear cool clothing and stay hydrated.\n");
+	}
+	else if(celsius <= 25.0 && celsius > 10.0){
+		printf("Temperature Category: Comftorable\nWeather Advisory: You should feel comftorable if you go outside now instead of playing on this computer all day.\n");
+	}
+	else if(celsius <= 10.0 && celsius >= 0.0){
+		printf("Temperature Category: Cold\nWeather Advisory: Wear a jacket if the you want to go outside rather than grade beginner C code.\n");
+	}
+	else{
+		printf("Temperature Category: Freezing\nWeather Advisory: Stay indoors--unless you want to be cool like polar bears and hang out in the freezing cold.\n");
+	}
 }
